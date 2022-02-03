55f045077d4a86066e5a2756af94cbdc61869cfb
janzcio <jmi050994@gmail.com>
2022-02-04 05:27:47 +0800

    [remove] prefix
    [fix seeder] add seeder ProductSeeder

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::group(['namespace' => 'Api\v1', 'prefix' => 'v1'], function() {
+Route::group(['namespace' => 'Api\v1', 'prefix' => '/'], function() {
     /*
     |--------------------------------------------------------------------------
     | Registration API
